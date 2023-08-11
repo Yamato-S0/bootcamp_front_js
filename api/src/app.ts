3aaf7af2d7501357019faa844e966a042eda808f
Yamato <shinohara559@gmail.com>
2023-08-11 10:31:29 +0900

    docs: second commit

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,4 +1,14 @@
 import express from "express";
+import { databaseManager } from "@/db/database_manager";
 
 export const app = express();
 
+//特定のURLにリクエストが来た時に、DBからタスクを取得してJSONで返すルーティングを作成
+app.get("/tasks", async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000')
+    //DBからタスクを取得
+    const db = await databaseManager.getInstance();
+    const result = await db.all("SELECT * FROM tasks");
+    //取得したタスクをJSONで返す
+    res.json(result);
+});
--- front/src/ts/application.ts
+++ front/src/ts/application.ts
@@ -1,5 +1,40 @@
 export class Application {
   start = async (): Promise<void> => {
+    //fetchなどを使ってAPI側に作ったルーティングにリクエストを送り、タスクを取得する
+    const response = await fetch(`${process.env.API_URL}/tasks`);
+    const tasks = await response.json();
+    //取得したタスクをDOM操作してHTMLに表示する
+    const ul = document.querySelector("ul");
+    //初期状態の2つのdummy taskを削除
+    if (ul) { 
+      while (ul.firstChild) {
+        ul.removeChild(ul.firstChild);
+      }
+    }
+    //tasksをidについて昇順にソート
+    tasks.sort((a: { id: number }, b: { id: number }) => {
+      return a.id - b.id;
+    });
+    
+    tasks.forEach((task: { id: number; title: any; status: number }) => {
+      const li = document.createElement("li");
+      if (task.status === 1) {
+        li.className = "task task--done";
+      } else {
+        li.className = "task task--todo";
+      }
+      const div1 = document.createElement("div");
+      div1.className = "task__btn";
+      const div2 = document.createElement("div");
+      div2.dataset.test = "task-title";
+      div2.className = "task__title";
+      div2.textContent = task.title;
+      li.appendChild(div1);
+      li.appendChild(div2);
+      if (ul) {
+        ul.appendChild(li);
+      }
+    });
     return;
   };
 }
